49938b930b1d70064528920492170ec79fee6d40
Brendon Choi <choi.brendon2001@gmail.com>
2023-06-17 09:26:15 +0900

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import Event from './Event'; // in src/EventList.js
+import Event from '../Event'; // in src/__tests__/EventList.test.js
 
 class EventList extends Component {
   render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+  const { events } = this.props;
+  return (
+    <ul className="EventList">
+      {events.map(event =>
+        <li>
+          <Event event={event} />
+        </li>
+      )}
+    </ul>
+  );
+}
 }
 
 export default EventList;
