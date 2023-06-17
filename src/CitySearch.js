bbbd1fc7c5e5fb0fc2377ddad3b777a03e2bb6f7
Brendon Choi <choi.brendon2001@gmail.com>
2023-06-17 09:37:10 +0900

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,17 +4,26 @@ class CitySearch extends Component {
   state = {
     query: '',
   }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  }
+
   render() {
-  return (
-    <div className="CitySearch">
-      <input
-        type="text"
-        className="city"
-      />
-      <ul className="suggestions">
-      </ul>
-    </div>
-  );
+    return (
+      <div className="CitySearch">
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
+        <ul className="suggestions">
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default CitySearch;
